4cd08c224ce1a47270f5a84105070090bdccaf1f
GeorgeAdrianC <george.adrian.ciobanu@gmail.com>
2022-07-31 18:22:44 +0300

    A simple fuel cost calculator.

---
+++ restaurants/main.py
@@ -0,0 +1,36 @@
+from restaurant_hw import Restaurant
+
+def main():
+    japanese_restaurant = Restaurant(
+        name = "kawasaki sushi",
+        cuisine = "japanese food",
+        # number_served = 33
+    )
+
+    chinease_restaurant = Restaurant(
+        name = "niam yam",
+        cuisine = "chinease food"
+    )
+
+    russian_restaurant = Restaurant(
+        name = "babushka's",
+        cuisine = "russian food",
+        # number_served = 16
+    )
+
+    print(japanese_restaurant.describe_restaurant())
+    print(japanese_restaurant.open_restaurant())
+    japanese_restaurant.set_number_served()
+    print(japanese_restaurant.restaurant()) 
+    japanese_restaurant.increment_number_served()
+    print(japanese_restaurant.restaurant()) 
+    print(chinease_restaurant.describe_restaurant())
+    print(chinease_restaurant.open_restaurant())
+    print(chinease_restaurant.restaurant())
+
+    print(russian_restaurant.describe_restaurant())
+    print(russian_restaurant.open_restaurant())
+    print(russian_restaurant.restaurant())
+     
+if __name__ == "__main__":
+    main()
---
+++ restaurants/restaurant_hw.py
@@ -0,0 +1,42 @@
+class Restaurant:
+    """A restaurant with italian cousine"""
+#metoda __init__:
+    def __init__(self, name: str, cuisine: str):
+        """Atributes that describe the restaurant"""
+        #atribute:
+        self.name = name
+        self.cuisine = cuisine
+        self.number_served = 0 
+#metode:
+    def describe_restaurant(self) -> str:
+        """Return the name and specific of the restaurant"""
+        name = f"{self.name}\n{self.cuisine}\n"       
+        return name.title()
+
+    def open_restaurant(self) ->str:
+        """Return a greeting phrase"""
+        its_opened = f"Welcome to {self.name.title()}!\nHere you will be served with the best {self.cuisine} in town!\n"
+        return its_opened
+
+    def restaurant(self) ->str:
+        """Return the number of customers served"""
+        served = f"So far were served {self.number_served} customers today\n      * * * * * * * * *\n"
+        return served
+    
+    def set_number_served(self) -> None:
+        """Let's us modify the number of customers served"""
+        while True:
+            try:
+                served = int(input(f"How many customers were served today at {self.name.title()}?: "))
+                self.number_served = served
+                break
+            except ValueError:
+                print("Please enter an integer.")
+    
+    def increment_number_served(self):
+        """Add clients to the number served"""
+        served = int(input("How many customers were served since last time?: "))
+        self.number_served = served + self.number_served
+       
+            
+
