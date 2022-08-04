4d0aa5831e9c5a431002a0ab49701252a07f0971
GeorgeAdrianC <george.adrian.ciobanu@gmail.com>
2022-08-04 11:35:01 +0300

    CPU and Memory usage monitoring program.

---
+++ system_monitoring/main.py
@@ -0,0 +1,29 @@
+import time
+import psutil
+
+
+def cpu_usage(cpu_usage, bars=50):
+    
+    cpu_percent = (cpu_usage / 100.0)
+    cpu_bar = "█" * int(cpu_percent * bars) + "-" * (bars - int(cpu_percent * bars))
+
+    print(f"\rCpu Usage: |{cpu_bar}| {cpu_usage: .2f}%  ", end="")
+
+
+def mem_usage(mem_usage, bars=50):
+    
+    mem_percent = (mem_usage / 100.0)
+    mem_bar = "█" * int(mem_percent * bars) + "-" * (bars - int(mem_percent * bars))
+    
+    print(f"Memory Usage: |{mem_bar}| {mem_usage: .2f}%  ", end="")
+
+
+def main():
+   
+    while True:
+        cpu_usage(psutil.cpu_percent(), 30)
+        time.sleep(0.5)
+        mem_usage(psutil.virtual_memory().percent, 30)
+
+if __name__ == "__main__":
+    main()
